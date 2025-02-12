8ca1d9028f97963c9aa2b62e996815dd5404e1a8
ybq-zhujun <106643533+ybq-zhujun@users.noreply.github.com>
2025-02-12 10:04:11 +0800

    Update HelloTest.java
    xiugai02

--- realtime-dws/src/main/java/HelloTest.java
+++ realtime-dws/src/main/java/HelloTest.java
@@ -11,5 +11,6 @@ public class HelloTest {
         System.out.println("master");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("xiugai02");
     }
 }
